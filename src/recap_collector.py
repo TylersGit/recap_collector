953cfec65f43033650a763e8cb90d07286db5e8c
Tylerk <1997.tylerjordan@gmail.com>
2024-04-15 20:08:16 +0300

    Adjusted time zone for today variable.

--- src/recap_collector.py
+++ src/recap_collector.py
@@ -13,8 +13,13 @@ SWID = os.environ["SWID"]
 ESPN_S2 = os.environ["ESPN_S2"]
 TEAM_ID = int(os.environ["TEAM_ID"])
 
-today = datetime.date.today().strftime("%m/%d/%Y")
-
+# Make sure that everything works on US time to prevent finicky game times.
+# It'll be easier to get the games if they start and end on the same day, rather
+# than start at 23:00 and end on 01:00 the next day.  
+# Use Chicago as reference point.
+# Go Cubs. 
+us_timezone = datetime.timezone(datetime.timedelta(hours=-5))
+today = datetime.datetime.today().astimezone(tz=us_timezone).strftime("%m/%d/%Y")
 
 league = League(league_id=LEAGUE_ID, year=YEAR, espn_s2=ESPN_S2, swid=SWID)
 schedule = statsapi.schedule(start_date=today, end_date=today)
@@ -27,5 +32,5 @@ playing_teams = playing_teams_getter(roster_players)
 relevant_games = relevant_games_getter(schedule, playing_teams)
 
 
-print(relevant_games)
+print(*relevant_games, sep="\n")
 # print(sched[0])
